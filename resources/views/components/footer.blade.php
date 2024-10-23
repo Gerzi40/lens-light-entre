ce58f4f68e1b623163b34c8d6c8701a52f9fa98d
Garry Nathanael <garry.nathanael@gmail.com>
2024-10-23 22:14:59 +0700

    pindahin file

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,31 @@
+<footer class="bg-[#BBA58F] pt-24 pb-12">
+    <div class="flex flex-wrap h-48">
+        <div class="flex w-1/4 items-center justify-center">
+            <a href="#" ><img src="/logo/logolens&lightnewbg.png" alt="OurLogo" class="h-24 w-30 mr-3"></a>
+        </div>
+        <div class="flex flex-col w-3/4"> 
+            <div class = "flex h-1/2">
+                <div class="bg-blue-600 w-1/3">
+                    <div class="ml-5 mt-2 justify-center">
+                        <h5 class="text-lg mb-1">Features</h5>
+                        <div class="flex flex-col text-xs">
+                            <a href="#">Home</a>
+                            <a href="#">About Us</a>
+                            <a href="#">Privacy Policy</a>
+                            <a href="#">FAQ</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-red-600 w-1/3">
+                    bbb
+                </div>
+                <div class="bg-green-600 w-1/3">
+                    ccc
+                </div>
+            </div>
+            <div class="w-full h-1/2 flex items-center justify-center">
+                <h3 class="text-neutral-600">© 2024 lens&light. All rights reserved.</h3>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,15 @@
+<header class="bg-slate-100">
+    <div class="flex justify-between h-30 ">
+        <div class="flex items-center">
+            <a href="#"><img src="/logo/logolens&lightnewbg.png" alt="OurLogo" class="h-20 w-30 mr-3"></a>
+            <a href="#" class="mr-3">Home</a>
+            <a href="" class="mr-3">Services</a>
+            <a href="" class="mr-3">Bookings</a>
+            <a href="" class="mr-3">About Us</a>
+        </div>
+        <div class="flex items-center">
+            <a href="" class="mr-3">Hi, John</a>
+            <a href=""><img src="/pp/marvell.png" alt="Orang" class="h-9 mr-6 rounded-3xl"></a>
+        </div>
+    </div>
+</header>
